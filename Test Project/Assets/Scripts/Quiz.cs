66c6cd9a47f9089492e0e9c570416db2d3ff19f4
Danuka De Alwis <danukaalwis@hotmail.com>
2022-05-07 14:54:19 +1200

    Adjusted logic and buttons

--- Test Project/Assets/Scripts/Quiz.cs
+++ Test Project/Assets/Scripts/Quiz.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 public class Quiz : MonoBehaviour
 {
     [SerializeField]private QuestionSO question;
-    [SerializeField] private TextMeshProUGUI textArea;
+    [SerializeField] private TextMeshProUGUI questionTextArea;
+    [SerializeField] private GameObject[] answerGroups;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        textArea.text = question.GetQuestion();
+        TextMeshProUGUI buttonText;
+        questionTextArea.text = question.GetQuestion();
+        for (int i = 0; i < answerGroups.Length; i++)
+        {
+            buttonText = answerGroups[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+        
     }
 
     // Update is called once per frame
