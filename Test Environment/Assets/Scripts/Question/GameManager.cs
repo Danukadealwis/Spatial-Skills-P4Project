d397773c3c043a1fa6123d7237d432aecea880a1
Danuka De Alwis <mdea606@aucklanduni.ac.nz>
2022-09-06 08:48:49 +1200

    Undo working correctly

--- Test Environment/Assets/Scripts/Question/GameManager.cs
+++ Test Environment/Assets/Scripts/Question/GameManager.cs
@@ -2,19 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.SceneManagement;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
     private List<int> _objectsSliced;
     private List<string> _fragmentRoots;
     private List<string> _slicedObjs;
     private int undoCount;
+    
+    private PlayerInput _playerInput;
+    private InputAction _undoAction;
 
     // Start is called before the first frame update
     void Start()
     {
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.SwitchCurrentActionMap("Player");
+        _undoAction = _playerInput.currentActionMap.FindAction("UndoCut");
         _objectsSliced = new List<int>();
         _fragmentRoots = new List<string>();
         _slicedObjs = new List<string>();
@@ -24,34 +31,44 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_undoAction.WasPressedThisFrame())
+        {
+            UndoCut();
+        }
+    }
+
+    public void NextQuestion()
+    {
         
     }
-    
-    public void UndoCut()  
+
+    public void UndoCut()
     {
         // Would have to know the name of the objects that were just created for the last cut and destroy them
         // pre req: For each cut, store the name of the cut object. 
-        
+
         // Also the name of the parent object and activate it again
         // for the
 
         
-        if (_slicedObjs.Count != 0)
-        {
-            var allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
-            GameObject obj = allObjects.Single(o => o.name == _slicedObjs.Last());
-            GameObject root = GameObject.Find(_fragmentRoots.Last()).gameObject;
- 
-            Debug.Log("Name of obj: " + obj.name);
-            Destroy(root.transform.gameObject);
-            obj.SetActive(true);
-            undoCount++;
-            _fragmentRoots.RemoveAt(_fragmentRoots.Count - 1);
-            _slicedObjs.RemoveAt(_slicedObjs.Count - 1);
-            _objectsSliced.RemoveAt(_objectsSliced.Count - 1);
-        }
+            if (_slicedObjs.Count != 0)
+            {
+                var allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+                GameObject obj = allObjects.Single(o => o.name == _slicedObjs.Last());
+                GameObject root = GameObject.Find(_fragmentRoots.Last()).gameObject;
+
+                Debug.Log("Name of obj: " + obj.name);
+                Destroy(root.transform.gameObject);
+                obj.SetActive(true);
+                undoCount++;
+                _fragmentRoots.RemoveAt(_fragmentRoots.Count - 1);
+                _slicedObjs.RemoveAt(_slicedObjs.Count - 1);
+                _objectsSliced.RemoveAt(_objectsSliced.Count - 1);
+            }
+
+
+            Debug.Log("Undo count:" + undoCount);
         
-        Debug.Log("Undo count:" + undoCount);
     }
 
     public void AddObjectSliced(int value, string fragmentRootName, string slicedObjName)
